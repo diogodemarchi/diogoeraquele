d91c4dfad2ea7a4c5ea4e7f8f3af40d1991c09a4
dod <diogo.demarchi@plus10.de>
2024-06-13 15:54:18 +0200

    refactor: change to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" />
         </Routes>
       </Router>
     </>
