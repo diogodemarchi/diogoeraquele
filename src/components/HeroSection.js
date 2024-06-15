c31383adb2d01203d46cf6e7413c001c1793878d
dod <diogo.demarchi@plus10.de>
2024-06-15 11:16:55 +0200

    refactor: omit root folder from video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted />
+      <video src="videos/video-2.mp4" autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <div>
         <FontAwesomeIcon icon={["fab", "facebook-f"]} />
