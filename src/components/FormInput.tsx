d15f87f6b5708d4a1feb2c824460c1702186d4ba
dod <diogo.demarchi@plus10.de>
2024-07-02 18:14:18 +0200

    fix: create forminput component

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -11,68 +11,34 @@ interface FormInputProps {
 
 const FormInput: React.FC<FormInputProps> = ({
   placeholder,
-  title,
-  type,
-  optional,
+  title = "",
+  type = "text",
+  optional = false,
   callback,
 }) => {
   const { t } = useTranslation();
-  let optionalInput = undefined;
-  let titleText = undefined;
-  const growInput = (
-    <input
-      type="text"
-      placeholder={t("type_here")}
-      className="grow"
-      onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-        callback(event.target.value)
-      }
-    />
-  );
-
-  if (title) {
-    // <label className="form-control w-full max-w-xs">
-    //   <div className="label">
-    //   <span className="label-text">What is your name?</span>
-    //   <span className="label-text-alt">Top Right label</span>
-    // </div>
-    // <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-    // <div className="label">
-    //   <span className="label-text-alt">Bottom Left label</span>
-    //   <span className="label-text-alt">Bottom Right label</span>
-    // </div>
-    // </label>
-    titleText = (
-      <div className="label">
-        <span className="label-text">{title}</span>
-      </div>
-    );
-  }
-
-  if (optional) {
-    optionalInput = <span className="badge badge-info">{t("optional")}</span>;
-  }
-
-  if (titleText || optionalInput) {
-    return (
-      <label className="input input-bordered input-primary text-black flex items-center gap-2 placeholder:text-black/70 bg-white">
-        {titleText}
-        {growInput}
-        {optionalInput}
-      </label>
-    );
-  }
 
   return (
-    <input
-      type={type ? type : "text"}
-      placeholder={placeholder}
-      className="input input-bordered input-primary w-full max-w-xs text-black placeholder:text-black/70 bg-white"
-      // this is for single column
-      // input input-bordered input-primary text-black flex items-center gap-2 placeholder:text-black/70 bg-white
-      // this is for multi column
-      // input input-bordered input-primary w-full max-w-xs text-black placeholder:text-black/70 bg-white
-    />
+    <div className="w-full">
+      {title && (
+        <div className="label mb-2">
+          <span className="label-text text-lg font-semibold text-[#4A07DA]">
+            {title}
+          </span>
+        </div>
+      )}
+      <label className="input input-bordered input-primary text-black flex items-center gap-2 bg-white">
+        <input
+          type={type}
+          placeholder={title ? t("type_here") : placeholder}
+          className="grow"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            callback(event.target.value)
+          }
+        />
+        {optional && <span className="badge badge-info">{t("optional")}</span>}
+      </label>
+    </div>
   );
 };
 
