34cd9d6d85351a78496b87e5a43256aa1a1f20d0
dod <diogo.demarchi@plus10.de>
2024-07-02 18:23:00 +0200

    refactor: add form component

--- src/components/pages/Rsvp.tsx
+++ src/components/pages/Rsvp.tsx
@@ -1,18 +1,10 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import FormInput from "../FormInput";
+import Form from "../Form";
 
 const Rsvp: React.FC = () => {
   const { t, i18n, ready } = useTranslation();
-
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [firstQuestion, setFirstQuestion] = useState("");
-  const [password, setPassword] = useState("");
-
-  const printState = () => console.log(phone);
 
   const formFieldStyle: string =
     "input input-bordered input-primary text-black flex items-center gap-2 placeholder:text-black/70 bg-white";
@@ -23,59 +15,7 @@ const Rsvp: React.FC = () => {
           <h1 className="text-center text-2xl sm:text-3xl font-semibold text-[#4A07DA]">
             {t("confirm_presence")}
           </h1>
-          <div className="w-full mt-5 sm:mt-8">
-            <div className="mx-auto w-full sm:max-w-md md:max-w-lg flex flex-col gap-5">
-              <div className="flex flex-col sm:flex-row gap-3">
-                <FormInput
-                  placeholder={t("first_name")}
-                  callback={setFirstName}
-                />
-                <FormInput
-                  placeholder={t("last_name")}
-                  callback={setLastName}
-                />
-              </div>
-              <FormInput
-                placeholder={t("email")}
-                type="email"
-                optional={true}
-                callback={setEmail}
-              />
-              <FormInput
-                placeholder={t("phone_number")}
-                type="tel"
-                optional={true}
-                callback={setPhone}
-              />
-              <FormInput
-                placeholder={t("type_here")}
-                title={t("some_first_question")}
-                optional={true}
-                callback={setFirstQuestion}
-              />
-              <div className="flex items-center gap-1.5  justify-start pl-2">
-                <div className="form-control">
-                  <label className="label cursor-pointer">
-                    <input
-                      type="checkbox"
-                      className="checkbox-xs checkbox-primary"
-                    />
-                  </label>
-                </div>
-                <h3 className="flex items-center whitespace-nowrap text-xs text-black">
-                  {t("agreement_text")}
-                </h3>
-              </div>
-              <div className="flex flex-col md:flex-row gap-2 md:gap-4 justify-center items-center">
-                <button className="btn btn-active btn-success btn-block max-w-[200px]">
-                  {t("confirm")}
-                </button>
-                <button className="btn btn-active btn-error btn-block max-w-[200px]">
-                  {t("deny")}
-                </button>
-              </div>
-            </div>
-          </div>
+          <Form />
         </div>
       </div>
     </div>
